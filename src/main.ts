54ae21e4eabd7ab35ad3e4ad6d5b6f13f9694fbe
jherson <rodriguezpaul98@gmail.com>
2025-01-24 11:53:22 -0400

    change main ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://partial1.netlify.app', // Dominio del frontend
+    methods: 'GET,POST,PUT,DELETE,OPTIONS', // Métodos HTTP permitidos
+    credentials: true, // Si necesitas enviar cookies o cabeceras autorizadas
+  });
+  
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
